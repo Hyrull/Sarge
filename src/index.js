4a4f3dd6bde64985afeb332882e77b2046672fa8
Hyrul <xavier.leonard@outlook.com>
2024-11-25 23:30:18 +0100

    added /event support

--- src/index.js
+++ src/index.js
@@ -202,6 +202,32 @@ client.on('interactionCreate', async (interaction) => {
         interaction.reply({files: [greetingsVideo]})
       }
 
+      if(interaction.commandName === "event") {
+        const roleId = '1095842303429587014'
+        const member = interaction.member
+        const hasRole = member.roles.cache.has(roleId)
+        const removeOption = interaction.options.getBoolean('set') || false
+
+        await interaction.reply({ content: `*Processing...*`, ephemeral: true })
+
+        if (!removeOption) {   
+          if (hasRole) {
+            await member.roles.remove(roleId)
+            interaction.editReply({ content: `The 'EventPing' role has been successfully removed.`})
+          } else {
+            interaction.editReply({ content: `You don't have the 'EventPing' role.`})
+          }
+        } else {
+          if (hasRole) {
+            interaction.editReply({ content: `You already have the 'EventPing' role!`})
+          } else {
+            await member.roles.add(roleId)
+            interaction.editReply({ content: `'EventPing' role successfully added!`})
+          }
+        }
+      }
+      
+
       if(interaction.commandName === "nsfw") {
         const member = interaction.member
         const lv20Role = '518961929583198209'
