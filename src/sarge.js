abb8436798137a699c75c3f4abd53a4b19e64430
Hyrul <xavier.leonard@outlook.com>
2025-06-12 03:42:48 +0200

    moved easteregg and autoban commands in their own file

--- src/sarge.js
+++ src/sarge.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const { Client, IntentsBitField, EmbedBuilder, MessageFlags } = require('discord.js')
 const http = require('http')
-const fsPromises = require('fs').promises
 const mongoose = require('mongoose')
 const { getSettings, updateSettingsInCache } = require('./utils/settingsManager')
 
@@ -15,8 +14,8 @@ const { pingCommand } = require('./slash-commands/ping')
 const { eventCommad } = require('./slash-commands/event')
 const { feedbackNotice } = require('./slash-commands/feedback')
 const { gptSearch } = require('./slash-commands/gpt-search')
-const incrementCount = require('./utils/incrementCount')
 const { default: messageCreateListener } = require('./listeners/messageCreate')
+const { checkEasterEggs, handleNsfwBan } = require('./slash-commands/nsfw')
 
 const greetingsVideo = './data/greetings.mp4'
 
@@ -32,7 +31,6 @@ const client = new Client ({
 
 //////////////////////////////////////////////////////////////////////////////////
 
-const easterEggsPath = './data/eastereggs.json'
 const startTime = Date.now()
 console.log(`Bot started at: ${new Date(startTime).toISOString()}`)
 
@@ -148,61 +146,10 @@ client.on('interactionCreate', async (interaction) => {
       }
       
       if(interaction.commandName === "nsfw") {
-        const member = interaction.member
-        const lv20Role = '518961929583198209'
-        const modLogsChannelId = '518821248768278528'
-        let easterEggTriggered = false
-        
-
-        // Reading ID to see if there's an easter egg associated with it
-        try {
-          const easterEggsData = await fsPromises.readFile(easterEggsPath, 'utf8')
-          const easterEggs = JSON.parse(easterEggsData)
-          const easterEgg = easterEggs.find(egg => egg.id === member.id)
-      
-          if (easterEgg) {
-            await interaction.reply(easterEgg.message)
-            easterEggTriggered = true
-          }
-        } catch (err) {
-          console.error('Error reading Easter eggs file:', err)
-        }
-        if (easterEggTriggered) return
-
-        // Default behavior
-
-        if (interaction.member.roles.cache.has(lv20Role)) {
-          interaction.reply("You are above level 20, so I'm saving you. Lucky you...")
-        } else {
-          await interaction.guild.members.ban(member, { reason: 'Fell for the /nsfw command'})
-          console.log(`Banned ${member} due to NSFW command usage.`)
-          incrementCount(message.guild.id, 'nsfwBans')
-
-
-          switch (settings.nsfwBans) {
-            case 1: {
-              await interaction.reply(`${member} is the ${settings.nsfwBans}st person to fall!! HURRAY!`)
-              break
-            }
-            case 2: {
-              await interaction.reply(`${member} is the ${settings.nsfwBans}nd person to fall...`)
-              break
-            }
-            case 3: {
-              await interaction.reply(`${member} is the ${settings.nsfwBans}rd person to fall...`)
-              break
-            }
-            default: {
-              await interaction.reply(`${member} is the ${settings.nsfwBans}th person to fall...`)
-            }
-          }
-
-          const modLogsChannel = interaction.client.channels.cache.get(modLogsChannelId)
-          if (modLogsChannel) {
-            await modLogsChannel.send(`${member} has been banned for /nsfw usage.`)
-          } else {
-             console.log(`Channel with ID '${modLogsChannelId}' not found.`)
-          }
+        const eggTriggered = await checkEasterEggs(interaction.member.id, interaction)
+        if (!eggTriggered) {
+          console.log(`${interaction.member.user.tag} used the /nsfw command and has no easter egg...`)
+          handleNsfwBan(interaction.member, interaction)
         }
       }
 
---
+++ src/slash-commands/nsfw.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const fs = require('fs').promises
+const incrementCount = require('../utils/incrementCount')
+
+const easterEggsPath = path.join(__dirname, '../../data/eastereggs.json')
+const lv20Role = '518961929583198209'
+const modLogsChannelId = '518821248768278528'
+
+
+
+
+
+async function checkEasterEggs(memberId, interaction) {
+  let easterEggTriggered = false
+  try {
+    const easterEggsData = await fs.readFile(easterEggsPath, 'utf8')
+    const easterEggs = JSON.parse(easterEggsData)
+    const easterEgg = easterEggs.find(egg => egg.id === memberId)
+
+    if (easterEgg) {
+      await interaction.reply(easterEgg.message)
+      easterEggTriggered = true
+    }
+  } catch (err) {
+    console.error('Error reading Easter eggs file:', err)
+  }
+  return easterEggTriggered
+}
+
+
+
+
+
+async function handleNsfwBan(member, interaction) {
+  if (interaction.member.roles.cache.has(lv20Role)) {
+    interaction.reply("You are above level 20, so I'm saving you. Lucky you...")
+  } else {
+    await interaction.guild.members.ban(member, { reason: 'Fell for the /nsfw command'})
+    console.log(`Banned ${member} due to NSFW command usage.`)
+    const nsfwBans = await incrementCount(interaction.guild.id, 'nsfwBans')
+
+    switch (nsfwBans) {
+      case 1: {
+        await interaction.reply(`${member} is the ${nsfwBans}st person to fall!! HURRAY!`)
+        break
+      }
+      case 2: {
+        await interaction.reply(`${member} is the ${nsfwBans}nd person to fall...`)
+        break
+      }
+      case 3: {
+        await interaction.reply(`${member} is the ${nsfwBans}rd person to fall...`)
+        break
+      }
+      default: {
+        await interaction.reply(`${member} is the ${nsfwBans}th person to fall...`)
+      }
+    }
+
+    const modLogsChannel = interaction.client.channels.cache.get(modLogsChannelId)
+    if (modLogsChannel) {
+      await modLogsChannel.send(`${member} has been banned for /nsfw usage.`)
+    } else {
+        console.log(`Channel with ID '${modLogsChannelId}' not found.`)
+    }
+  }
+}
+
+module.exports = {
+  checkEasterEggs,
+  handleNsfwBan
+}
